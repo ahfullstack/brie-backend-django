7d90c4ee754640e6a7af68cb6152a7432a493c85
ahfullstack <ahfullstack@gmail.com>
2022-04-29 20:19:54 -0700

    auth

--- brie/settings.py
+++ brie/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'status_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'brie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'p4',
+        'USER': 'postgres',
+        'PASSWORD': 'ganggang',
+        'HOST': 'localhost'
     }
 }
 
