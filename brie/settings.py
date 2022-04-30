7172b078750f283bcf749f74be7fda93a3ceec2c
ahfullstack <ahfullstack@gmail.com>
2022-04-29 20:30:34 -0700

    model completed

--- brie/settings.py
+++ brie/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'status_app',
+    'status_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- status_api/models.py
+++ status_api/models.py
@@ -1,3 +1,8 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(null=True, blank=True,max_length=32)
+    description = models.CharField(null=True, blank=True,max_length=200)
+    symptoms = ArrayField(ArrayField(models.IntegerField()))
